7ae83a8c7cf118abeccd88de1f0f7a3febea630b
HenryKTLe <102492293+HenryKTLe@users.noreply.github.com>
2022-09-14 11:07:59 +1000

    moved?

--- InventoryService/InventoryService/src/main/java/group09/InventoryService/Repositories/PartRepository.java
+++
@@ -1,9 +0,0 @@
-package group09.InventoryService.Repositories;
-
-import group09.InventoryService.Entities.Part;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartRepository extends JpaRepository<Part, Long> {
-}
--- InventoryService/InventoryService/src/main/java/group09/InventoryService/Repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package group09.InventoryService.Repositories;
-
-import group09.InventoryService.Entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
