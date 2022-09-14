741e48670107bbaef908839fbea030cc21e9a727
LiamBubb <103977348+LiamBubb@users.noreply.github.com>
2022-09-14 11:18:49 +1000

    Add files via upload

---
+++ SalesService/src/main/java/group09/SalesService/Services/SalesService.java
@@ -0,0 +1,61 @@
+package group09.SalesService.Services;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import group09.SalesService.Entities.Sale;
+import group09.SalesService.Repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+
+import java.sql.SQLOutput;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class SalesService {
+    private final SaleRepository saleRepository;
+
+    @Autowired
+    private final RestTemplate restTemplate;
+
+    public SalesService(SaleRepository saleRepository, RestTemplate restTemplate) {//, RestTemplate restTemplate) {
+        this.saleRepository = saleRepository;
+        this.restTemplate = restTemplate;
+    }
+
+    public List<Sale> getSales() {
+        return saleRepository.findAll();
+    }
+
+    public List<Sale> getPart(Long id) {
+        return saleRepository.findAllById(Collections.singleton(id));
+    }
+
+    public Sale createSale(Sale sale) {
+        return saleRepository.save(sale);
+    }
+
+    public Optional<Sale> updateSale(Sale newSale, Long id) {
+        return saleRepository.findById(id)
+                .map(sale -> {
+                    sale.setProductName(newSale.getProductName());
+                    sale.setQuantity(newSale.getQuantity());
+                    sale.setSupplierId(newSale.getSupplierId()); //new addition
+                    return saleRepository.save(sale);
+                });
+    }
+
+    public void deleteSale(Long id) {
+        saleRepository.deleteById(id);
+    }
+}
