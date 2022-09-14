35b42d8ba44c7c2180be0caf732e4c29e6c553b3
HenryKTLe <102492293+HenryKTLe@users.noreply.github.com>
2022-09-14 13:29:02 +1000

    Delete SalesService/SalesService directory

--- SalesService/SalesService/src/main/java/group09/SalesService/Repositories/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package group09.SalesService.Repositories;
-
-import group09.SalesService.Entities.Sale;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleRepository extends JpaRepository<Sale, Long> {
-}
