23bf0bc5fd042cc45bce6ada2ae355a5901b1c4c
HenryKTLe <102492293+HenryKTLe@users.noreply.github.com>
2022-09-14 12:08:54 +1000

    Liam_SalesService_added
    12:08pm_14/09/2022
    
    modified folder by Henry

---
+++ SalesService/SalesService/src/main/java/group09/SalesService/Controllers/SaleController.java
@@ -0,0 +1,46 @@
+package group09.SalesService.Controllers;
+
+import group09.SalesService.Entities.Sale;
+import group09.SalesService.Services.SalesService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+@RequestMapping(path = "/sale")
+public class SaleController {
+    private final SalesService salesService;
+
+    public SaleController(SalesService salesService) {
+        this.salesService = salesService;
+    }
+
+
+    @GetMapping
+    List<Sale> getSales(){
+        return salesService.getSales();
+    }
+
+    @PostMapping
+    Sale createSale(@RequestBody Sale sale)
+    {
+        return salesService.createSale(sale);
+    }
+
+    @PutMapping(path = "{id}")
+    public Optional<Sale> updatePart(@RequestBody Sale newSale, @PathVariable("id") Long id)
+    {
+        return salesService.updateSale(newSale, id);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteSale(@PathVariable("id") Long id)
+    {
+        salesService.deleteSale(id);
+    }
+}
---
+++ SalesService/SalesService/src/main/java/group09/SalesService/Repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package group09.SalesService.Repositories;
+
+import group09.SalesService.Entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
